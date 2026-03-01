ee8921a8e1514ccbeb48f554d64a0e302acadfa1
Amadeus Mader <amadeus@mozart409.com>
2026-03-01 21:10:15 +0100

    feat(tools): add flake

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Development environment for pinchflat";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in {
+      # nix develop . --command fish
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          lazydocker
+          lefthook
+          cocogitto
+          just
+          tailwindcss_4
+          docker
+          docker-buildx
+          docker-compose
+          opencode
+          typos
+        ];
+        shellHook = ''
+          lefthook install
+          cog install-hook
+          export COMPOSE_BAKE=true
+        '';
+      };
+    });
+}
